bdf8513d3a09846524327894b97bde76d4b99602
emanbasahel <Eman0500821725>
2019-01-17 20:42:14 +0300

    arrange and link UI with data

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -2,26 +2,45 @@ package com.udacity.sandwichclub;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
+
     private static final int DEFAULT_POSITION = -1;
+
+    ImageView ingredientsIv;
+
+    TextView txtOrigin, txtDescription, txtKnownAs;
+
+    Sandwich sandwich;
+
+    ListView ingredientListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
-
+        ingredientsIv = findViewById(R.id.image_iv);
+        txtOrigin = findViewById(R.id.origin_tv);
+        txtDescription = findViewById(R.id.description_tv);
+        txtKnownAs = findViewById(R.id.also_known_tv);
+        ingredientListView = findViewById(R.id.ingredients_tv);
+        ViewCompat.setNestedScrollingEnabled(ingredientListView, true);
         Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
@@ -36,7 +55,7 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        sandwich = JsonUtils.parseSandwichJson(json);
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
@@ -57,6 +76,26 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
+        txtOrigin.setText(sandwich.getPlaceOfOrigin());
+        txtDescription.setText(sandwich.getDescription());
+        List<String> alsoKnownList = sandwich.getAlsoKnownAs();
+        StringBuilder stringBuilder = new StringBuilder();
+        if (alsoKnownList != null) {
+            for (int i = 0; i < alsoKnownList.size(); i++) {
+                stringBuilder.append(sandwich.getAlsoKnownAs().get(i));
+                stringBuilder.append(", ");
+            }
+            txtKnownAs.setText(stringBuilder);
+        }
+        if (sandwich.getIngredients() != null) {
+        List<String> ingredientList = sandwich.getIngredients();
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                    this,
+                    android.R.layout.simple_list_item_1,
+                    ingredientList);
+
+            ingredientListView.setAdapter(arrayAdapter);
+        }
 
     }
 }
